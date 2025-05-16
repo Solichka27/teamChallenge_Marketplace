252e07dd4d8e56d7094f631c1c3d62cc0ddb100a
Mariia <myshyck@gmail.com>
2025-05-16 13:46:16 +0300

    add profile page

--- app/(tabs)/profile.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function ProfilePage() {
-  return (
-    <View>
-      <Text>Сторінка профілю</Text>
-    </View>
-  );
-}
---
+++ app/(tabs)/profile/addresses.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function MyAddresses() {
+  return (
+    <View>
+      <Text>Мої адреси</Text>
+    </View>
+  );
+}
