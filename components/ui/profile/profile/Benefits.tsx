339b404afbe3e8788bdc379e7a6831d8f4840d82
Mariia <myshyck@gmail.com>
2025-05-19 13:20:51 +0300

    add navigation for profile pages

--- components/ui/profile/profile/Benefits.tsx
+++ components/ui/profile/profile/Benefits.tsx
@@ -1,16 +1,30 @@
 import { StyleSheet, Text, View } from "react-native";
 import { PiggyBank, UsersThree } from "phosphor-react-native";
 
+// заглушка
+const bonusCount = 54;
+const subscriptionsCount = 28;
+
+const formatCount = (num: number) => {
+  if (num >= 1_000_000) {
+    return `${Math.floor(num / 1_000_000)}м`;
+  }
+  if (num >= 1_000) {
+    return `${Math.floor(num / 1_000)}т`;
+  }
+  return num.toString();
+};
+
 export default function Benefits() {
   return (
     <View style={styles.benefitsBlock}>
       <View style={styles.benefits}>
         <PiggyBank size={32} weight="thin" color="#8E6CEF" />
-        <Text style={styles.text}>Бонуси (54 ₴)</Text>
+        <Text style={styles.text}>Бонуси ({formatCount(bonusCount)} ₴)</Text>
       </View>
       <View style={styles.benefits}>
         <UsersThree size={32} weight="thin" color="#8E6CEF" />
-        <Text style={styles.text}>Підписки (28)</Text>
+        <Text style={styles.text}>Підписки ({formatCount(subscriptionsCount)})</Text>
       </View>
     </View>
   );
