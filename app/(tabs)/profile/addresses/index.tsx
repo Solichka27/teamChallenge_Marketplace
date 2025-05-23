d98b7f6b7e3ef9e98c4c254b17126770f7e8b0a1
Solichka27 <113928269+Solichka27@users.noreply.github.com>
2025-05-23 11:16:38 +0300

    import MyAddress

--- app/(tabs)/profile/addresses/index.tsx
+++ app/(tabs)/profile/addresses/index.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, ScrollView, SafeAreaView } from 'react-native';
-import MyAddresses from '@/components/ui/profile/address/MyAddress';
+import MyAddress from '@/components/ui/profile/address/MyAddress';
 
 export default function Addresses() {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
       <ScrollView contentContainerStyle={styles.container}>
-        <MyAddresses />
+        <MyAddress />
       </ScrollView>
     </SafeAreaView>
   );
